6414ac4869f170ac355d9fead93e6947435367eb
Sangwan Han <jklom0326@gmail.com>
2021-07-10 01:57:52 +0900

    chapter04-calculator

--- aoppart2chapter04/app/src/main/java/fastcampas/aop/part2/aop_part2_chapter_04/MainActivity.kt
+++ aoppart2chapter04/app/src/main/java/fastcampas/aop/part2/aop_part2_chapter_04/MainActivity.kt
@@ -2,10 +2,25 @@ package fastcampas.aop.part2.aop_part2_chapter_04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun buttonClicked(v: View) {
+
+    }
+    fun resultButtonClicked(v: View) {
+
+    }
+    fun historyButtonClicked(v: View) {
+
+    }
+    fun clearButtonClicked(v: View) {
+
+    }
+
 }
