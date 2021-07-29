44bca06e3e6aded494a0a9c365eef73b28c72fd7
Sangwan Han <jklom0326@gmail.com>
2021-07-29 23:04:09 +0900

    aop

--- aoppart3chapter01/app/src/main/java/fastcampas/aop/part2/aop_part3_chapter01/MyFirebaseMessagingService.kt
+++ aoppart3chapter01/app/src/main/java/fastcampas/aop/part2/aop_part3_chapter01/MyFirebaseMessagingService.kt
@@ -1,9 +1,15 @@
 package fastcampas.aop.part2.aop_part3_chapter01
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
     /*
     새 기기에서 앱복원
     사용자가 앱삭제/ 재설치  ==> 토큰이 초기화 될 수 있기때문에
@@ -14,7 +20,42 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         super.onNewToken(p0)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remotemessage: RemoteMessage) {
+        super.onMessageReceived(remotemessage)
+
+        createNotificationChannel()
+
+        val title = remotemessage.data["title"]
+        val message = remotemessage.data["message"]
+
+        val notificationBuilder =  NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        NotificationManagerCompat.from(this)
+            .notify(1,notificationBuilder.build() )
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            channel.description = CHANNEL_DESCRIPTION
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
     }
 }
