ded4104112f1831991e1c268792927ec71e3dbba
Sangwan Han <jklom0326@gmail.com>
2021-08-04 22:23:51 +0900

    aop

---
+++ aoppart3chapter03/app/src/main/java/fastcampas/aop/part2/aop_part3_chapter03/AlarmDisplayModel.kt
@@ -0,0 +1,24 @@
+package fastcampas.aop.part2.aop_part3_chapter03
+
+data class AlarmDisplayModel(
+    val hour: Int,
+    val minute: Int,
+    var onOff: Boolean
+) {
+    val timeText: String
+        get() {
+            val h = "%02d".format(if (hour < 12) hour else hour - 12)
+            val m = "%02d".format(minute)
+
+            return "$h : $h"
+        }
+
+    val ampmText: String
+        get() {
+            return if (hour < 12) "AM" else "PM"
+        }
+
+    fun makeDataForDB(): String {
+        return "$hour:$minute"
+    }
+}
