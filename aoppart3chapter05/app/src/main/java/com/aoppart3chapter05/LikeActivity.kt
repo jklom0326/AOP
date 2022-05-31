25edf530e9d2bc470db5d3de3f516909bd1edf21
jklom0326 <jklom0326@gmail.com>
2022-05-31 23:00:27 +0900

    feat.Regist & Login

---
+++ aoppart3chapter05/app/src/main/java/com/aoppart3chapter05/LikeActivity.kt
@@ -0,0 +1,13 @@
+package com.aoppart3chapter05
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LikeActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_like)
+    }
+
+}
--- aoppart3chapter05/app/src/main/java/com/aoppart3chapter05/LoginActivity.kt
+++ aoppart3chapter05/app/src/main/java/com/aoppart3chapter05/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.aoppart3chapter05
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
@@ -7,6 +8,12 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -14,12 +21,14 @@ import com.google.firebase.ktx.Firebase
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = Firebase.auth
+        callbackManager = CallbackManager.Factory.create()
 
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
@@ -27,6 +36,7 @@ class LoginActivity : AppCompatActivity() {
         initLoginButton()
         initSignUpButton()
         initEmailAndPasswordEditText()
+        initFacebookLoginButton()
 
     }
 
@@ -88,7 +98,38 @@ class LoginActivity : AppCompatActivity() {
             signUpButton.isEnabled = enable
         }
     }
+    private fun initFacebookLoginButton() {
+        val facebookLoginButton = findViewById<LoginButton>(R.id.facebookloginbutton)
+
+        facebookLoginButton.setPermissions("email", "public_profile")
+        facebookLoginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult) {
+                // 로그인이 성공적
+                val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this@LoginActivity) { task ->
+                        if (task.isSuccessful) {
+                            finish()
+                        } else {
+                            Toast.makeText(this@LoginActivity, "페이스북 로그인이 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+            override fun onCancel() {}
+
+            override fun onError(error: FacebookException?) {
+                Toast.makeText(this@LoginActivity, "페이스북 로그인이 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
 
     private fun getInputEmail() = findViewById<EditText>(R.id.emailEditText).text.toString()
     private fun getInputPassword() = findViewById<EditText>(R.id.passwordEditText).text.toString()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
 }
