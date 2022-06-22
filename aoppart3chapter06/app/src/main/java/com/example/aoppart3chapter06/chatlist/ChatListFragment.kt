15460f6cc9bcfa7f5db5ae93aba1abcb8a82a8a8
jklom0326 <jklom0326@gmail.com>
2022-06-23 02:42:45 +0900

    feat. fragment

---
+++ aoppart3chapter06/app/src/main/java/com/example/aoppart3chapter06/chatlist/ChatListFragment.kt
@@ -0,0 +1,7 @@
+package com.example.aoppart3chapter06.chatlist
+
+import androidx.fragment.app.Fragment
+import com.example.aoppart3chapter06.R
+
+class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
+}
---
+++ aoppart3chapter06/app/src/main/java/com/example/aoppart3chapter06/mypage/MyPageFragment.kt
@@ -0,0 +1,7 @@
+package com.example.aoppart3chapter06.mypage
+
+import androidx.fragment.app.Fragment
+import com.example.aoppart3chapter06.R
+
+class MyPageFragment: Fragment(R.layout.fragment_mypage) {
+}
