d0964349627e5c0d5a801dd8c4eed7f06ecc40c3
jklom0326 <jklom0326@gmail.com>
2022-09-08 23:43:55 +0900

    fragment & mainActivitiy motionlayout

--- aoppart4chapter01/app/src/main/java/com/example/aoppart4chapter01/MainActivity.kt
+++ aoppart4chapter01/app/src/main/java/com/example/aoppart4chapter01/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, PlayerFragment())
+            .commit()
     }
 }
